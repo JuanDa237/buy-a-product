cac47f6dd50082cb27c2632090cbbdc34a300b54
Juan David Gaviria Correa <juandavid200301@hotmail.com>
2026-05-02 11:29:09 -0500

    build(orders): add postgres connection

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// App
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+
+// Libs
 import { HealthModule } from '@app/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get<string>('POSTGRES_HOST', 'localhost'),
+                port: configService.get<number>('POSTGRES_PORT', 5432),
+                username: configService.get<string>('POSTGRES_USER', 'postgres'),
+                password: configService.get<string>('POSTGRES_PASSWORD', 'postgres'),
+                database: configService.get<string>('POSTGRES_DB', 'orders'),
+                entities: [],
+                synchronize: configService.get<string>('NODE_ENV', 'development') === 'development',
+            }),
+            inject: [ConfigService],
+        }),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
